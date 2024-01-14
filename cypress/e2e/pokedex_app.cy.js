498c52902f3d75095c5171b3977669f38587f189
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2024-01-14 16:29:42 +0100

    11.9 Simple end to end tests

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -1,9 +1,17 @@
 /* eslint-disable no-undef */
-describe('Pokedex', function()
+describe('Pokedex', () =>
 {
-  it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+  it('front page can be opened', () =>
+  {
+    cy.visit('')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('pokemon page shows the right content', () =>
+  {
+    cy.visit('')
+    cy.contains('charmander').click()
+    cy.contains('solar power')
   })
 })
