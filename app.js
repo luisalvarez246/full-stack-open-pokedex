9f6db37c5412deddbe7f2dc9e777d5c25f8285ce
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2024-01-18 18:20:11 +0100

    restrict delployment in PR

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('SPECIAL BUMB CANON!! PR Request')
+  res.send('SPECIAL BUMB CANON!! PR Request, deploy just from main')
 })
 
 app.get('/health', (req, res) => {
