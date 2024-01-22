d77aeefed2b86b889d0e85c0af7306ee0ed14db0
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2024-01-22 12:26:05 +0100

    11.18 format slack messages

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('SPECIAL BUMB CANON!! try slack notifications')
+  res.send('SPECIAL BUMB CANON!! format slack notifications')
 })
 
 app.get('/health', (req, res) => {
